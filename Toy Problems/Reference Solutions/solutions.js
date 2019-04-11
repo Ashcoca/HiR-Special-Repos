c7edf069e6aa2122b2d5bf6ab7ff19a7e078b95a
Ashvin Coca <AshvinCoca@gmail.com>
2019-04-10 20:04:51 -0700

    Cleaned up code, added more notes

--- Toy Problems/Reference Solutions/solutions.js
+++ Toy Problems/Reference Solutions/solutions.js
@@ -5,8 +5,8 @@
 function Calculator() {
   this.add = function(input){
     return function() {
-      var result = 0;
-      for (var i = 0; i < input.length; i++) {
+      var result = input[0];
+      for (var i = 1; i < input.length; i++) {
         result = result + input[i];
       };
       return result;
@@ -15,8 +15,8 @@ function Calculator() {
 
   this.multiply = function(input){
     return function() {
-      var result = 1;
-      for (var i = 0; i < input.length; i++) {
+      var result = input[0];
+      for (var i = 1; i < input.length; i++) {
         result = result * input[i];
       };
       return result;
@@ -24,6 +24,7 @@ function Calculator() {
   };
 
   // Instead of writing more repetitive code like above let's try use a helper function to save time!
+  // When you notice your code is repetitive it can be a good idea to try and refactor!
   this.subtract = function(input){
     return operation(input, function(result, input) { 
       return result - input; 
@@ -37,8 +38,8 @@ function Calculator() {
   };
 
   function operation(input, operator) {
-    var result = input.shift();
-    for(var i = 0; i < input.length; i++){
+    var result = input[0];
+    for(var i = 1; i < input.length; i++){
       result = operator(result, input[i]);
     };
     return result;
