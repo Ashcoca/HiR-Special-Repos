9dd6061cd596d050b1e81db1a299b59917ea833f
Ashvin Coca <AshvinCoca@gmail.com>
2019-04-11 10:28:47 -0700

    Added this worksheet problems, currently adding solutions

---
+++ Toy Problems/Reference Solutions/thisWorksheet.js
@@ -0,0 +1,156 @@
+// * Problem 1
+// whatIsThis('hello', 'world');
+// * "this" is ...    Window Object
+// * because ...      this is being called globally
+
+
+
+
+// * Problem 2
+// window.whatIsThis('hello', 'world');
+// * "this" is ...    Window Object
+// * because ...      The window object is being explicitly called
+
+
+
+
+// * Problem 3
+// inAnObject.test1('face', 'book');
+// * "this" is ...    the inAnObject obj
+// * because ...      Whenever a function is called by a preceding dot, the object before that dot is bound to this
+
+
+
+
+
+
+// * Problem 4
+// inAnObject.anotherObject.test1('twitter', 'book');
+// * "this" is ...    an Error
+// * because ...      There is no test1 method in the anotherObject
+
+
+
+
+// * Problem 5
+// inAnObject.anotherObject.test2('twitter', 'book');
+// * "this" is ...    anotherObject
+// * because ...      When a function is called (in this case test2), by a preceding dot, the object before 
+//                    that dot (in this case anotherObject) is "this."
+
+
+
+
+// * Problem 6
+// whatIsThis.call();
+// * "this" is ...    Window
+// * because ...      When you use call, "this" will usually be applied to the first parameter. 
+//                    Since this has no parameter, it will be applied to the "whatIsThis" function, which makes 'this' the window object
+
+
+
+
+
+// * Problem 7
+// whatIsThis.call(trickyTricky);
+// * "this" is ...    trickyTricky Object
+// * because ...      Whenever JavaScript’s call or apply method is used, this is explicitly defined as the trickyTricky obj
+//                    
+
+
+
+
+// * Problem 8
+// whatIsThis.call(trickyTricky, 'nice', 'job');
+// * "this" is ...    trickyTricky Object
+// * because ...      'this' is bound to the first parameter. The additional parameters will be used as arguments for the whatIsThis function
+
+
+
+
+// * Problem 9
+// whatIsThis.call(confusing);
+// * "this" is ...    confusing object
+// * because ...      Similar to the above, call applies to the argument
+
+
+
+
+// * Problem 10
+// whatIsThis.call(confusing, 'hello');
+// * "this" is ...    confusing object
+// * because ...      call applies this to the argument, this must always be an Object
+
+
+
+
+// * Problem 11
+// whatIsThis.apply(trickyTricky);
+// * "this" is ... trickyTricky
+// * because ... apply does the same thing that call does
+
+
+
+
+// * Problem 12
+// whatIsThis.apply(confusing, ['nice', 'job']);
+// * "this" is ... confusing object
+// * because ... apply applies this to the argument, this must always be an Object
+
+
+
+
+// * Problem 13
+// whatIsThis.apply(confusing, 'nice', 'job');
+// * "this" is ... Error
+// * because ... apply wants to only deal with objects
+
+
+
+
+// * Problem 14
+// inAFunction('what will', 'happen?');
+// * "this" is ... Window
+// * because ... this must always be an Object, so it defaults to the Window
+
+
+
+
+// * Problem 15
+// inAFunction.test3('A', 'B');
+// * "this" is ... Error
+// * because ... No function exists
+
+
+
+
+// * Problem 16
+// var newObject = new inAFunction('what will', 'happen?');
+// * "this" is ... Window
+// * because ... this must always be an Object
+
+
+
+
+// * Problem 17
+// var newObject = new inAFunction('what will', 'happen?');
+// newObject.test3('C', 'D');
+// * "this" is ... first Window then the inAFunction
+// * because ... first this is Window, then a new object is created and this
+// 								becomes the focus of that
+
+
+
+
+// * Problem 18
+// inAnObject.test1.call(trickyTricky, 'face', 'book');
+// * "this" is ... trickyTricky object
+// * because ... call applies this to the argument, this must always be an Object
+
+
+
+
+// * Problem 19
+// inAnObject.anotherObject.test2.apply(confusing, ['foo', 'bar']);
+// * "this" is ... confusing object
+// * because ... apply applies this to the argument, this must always be an Object
