dfe0d4a9dc94ca4dae90d1b8f177718c46a8c276
Ashvin Coca <AshvinCoca@gmail.com>
2019-04-10 20:10:35 -0700

    Tweaked spec

--- Toy Problems/spec/closureScopeThisPractics.spec.js
+++ Toy Problems/spec/closureScopeThisPractics.spec.js
@@ -9,25 +9,25 @@ describe("Calculator", function() {
       expect(calculator).not.to.equal(null);
      });
 
-    it("adds numbers together properly", function() {
-      expect(calculator.add([1,0])).to.equal(1);
+    it("Should add numbers", function() {
+      expect(calculator.add([1, 0])).to.equal(1);
       expect(calculator.add([5, 6])).to.equal(11);
       expect(calculator.add([1,2,3])).to.equal(6);
     });
 
-    it("multiplies numbers properly", function(){
-      expect(calculator.multiply([1,2])).to.equal(2);
+    it("Should multiply numbers", function() {
+      expect(calculator.multiply([1, 2])).to.equal(2);
       expect(calculator.multiply([5, 6, 7])).to.equal(210);
       expect(calculator.multiply([5, 0, 6])).to.equal(0);
     });
 
-    it("subtracts numbers properly", function(){
-      expect(calculator.subtract([3,2])).to.equal(1);
+    it("Should subtract numbers", function() {
+      expect(calculator.subtract([3, 2])).to.equal(1);
       expect(calculator.subtract([5, 4, 1])).to.equal(0);
     });
 
-    it("returns the proper quotient of the numbers passed in", function(){
-      expect(calculator.divide([10,2,5])).to.equal(1);
+    it("Should divide numbers", function() {
+      expect(calculator.divide([10, 2, 5])).to.equal(1);
       expect(calculator.divide([40, 2])).to.equal(20);
       expect(calculator.divide([0, 2])).to.equal(0);
     });
