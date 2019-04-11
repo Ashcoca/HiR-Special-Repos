74c54818d962e35f10b236581848e6910f0b10cf
Ashvin Coca <AshvinCoca@gmail.com>
2019-04-10 20:11:10 -0700

    Fixed phrasing on tests

--- Toy Problems/spec/closureScopeThisPractics.spec.js
+++ Toy Problems/spec/closureScopeThisPractics.spec.js
@@ -9,24 +9,24 @@ describe("Calculator", function() {
       expect(calculator).not.to.equal(null);
      });
 
-    it("Should add numbers", function() {
+    it("should add numbers", function() {
       expect(calculator.add([1, 0])).to.equal(1);
       expect(calculator.add([5, 6])).to.equal(11);
       expect(calculator.add([1,2,3])).to.equal(6);
     });
 
-    it("Should multiply numbers", function() {
+    it("should multiply numbers", function() {
       expect(calculator.multiply([1, 2])).to.equal(2);
       expect(calculator.multiply([5, 6, 7])).to.equal(210);
       expect(calculator.multiply([5, 0, 6])).to.equal(0);
     });
 
-    it("Should subtract numbers", function() {
+    it("should subtract numbers", function() {
       expect(calculator.subtract([3, 2])).to.equal(1);
       expect(calculator.subtract([5, 4, 1])).to.equal(0);
     });
 
-    it("Should divide numbers", function() {
+    it("should divide numbers", function() {
       expect(calculator.divide([10, 2, 5])).to.equal(1);
       expect(calculator.divide([40, 2])).to.equal(20);
       expect(calculator.divide([0, 2])).to.equal(0);
